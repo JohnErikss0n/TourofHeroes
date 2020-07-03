363a58b15abc2ee38e59543e133f589f13e39b54
John Eriksson <johneriksson2000@gmail.com>
2020-07-03 13:44:33 -0400

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Doom' },
+  { id: 12, name: 'Pen Man' },
+  { id: 13, name: 'Intensima' },
+  { id: 14, name: 'Magneto' },
+  { id: 15, name: 'Anteater Man' },
+  { id: 16, name: 'RubberWoman' },
+  { id: 17, name: 'The Dude' },
+  { id: 18, name: 'King Kong' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
